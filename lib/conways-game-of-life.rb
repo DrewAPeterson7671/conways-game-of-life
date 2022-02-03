766a8db2f00408eed83fba57a660077c19482861
Drew Peterson <drew.a.peterson@hotmail.com>
2022-02-03 00:43:38 -0800

    solid start of the generate_next_gen method, that will provide the flow.  Most methods should run inside of it.  Working on neighborhood_scan method.  Basic concept is to provide a template array that gives a generic perimeter.  Variable current_zone finds the current coordinates.  Those will be added to each array in scan_template, creating the coordinates to scan the perimeter of a zone.  Variable filter_template will remove all coordinates that mathematically put it outside the grid.  Troubleshooting that functionality now.

--- lib/conways-game-of-life.rb
+++ lib/conways-game-of-life.rb
@@ -1,111 +1,84 @@
 
-
 # lets display the grid first and see how that looks
 # remember that the off-by-one error is a factor here
 
-board_grid = [["O","X","X"], ["X","O","O"], ["O","O","X"]]
+# scan_template = [[-1, 1], [0, 1], [1, 1],
+# [-1, 0], [0, 0], [1, 0],
+# [-1, -1], [0, -1], [1, -1]]
 
-new_board_grid = {1 => [-1, 1, "O"], 2 => [0, 1, "X"], 3 => [1, 1, "X"], 4 => [-1, 0, "X"], 5 => [0, 0, "O"], 6 => [1, 0, "O"], 7 => [-1, -1, "O"], 8 => [0, -1, "O"], 9 => [1, -1, "X"]}
+board_grid = { "-1, 1" => "O", "0, 1" => "X", "1, 1" => "X", "-1, 0" => "X", "0, 0" => "O", "1, 0" => "O", "-1, -1" => "O", "0, -1" => "O", "1, -1" => "X" }
 
-top_row = ""
-mid_row = ""
-bottom_row = ""
+g1_top_row = ""; g1_mid_row = ""; g1_bottom_row = ""
+top_row = ""; mid_row = ""; bottom_row = ""
 
 def generate_next_gen(grid)
-  # I'd prefer not to destructively alter the original board_grid
+  # I'd prefer not to destructively alter the original board_grid for visual verfication
   # I don't like using "gen1_grid" as a name, wanted to make it flexible for multiple generation uses
   @gen1_grid = Marshal.load(Marshal.dump(grid))
-  @gen1_grid.each do |g1|
-    # g1.map do |i|
-      puts g1 # check neighbors if 1st zone
-      # if method = true : change status
-      # Another way to look at it is O is baseline?
-    # end
-    
-    
-    
+  @gen1_grid.each do |key, value|
+    puts "#{key} \t #{value}"
+    neighbor_scan(key)
+    break
+    # neighbor_scan(key)
   end
-  # puts ""
-  # puts @gen1_grid
-  # puts ""
-end
-
-def corner_scan(orientation)
-  # ok, shift zero to center
-  # Then remove any "2"
-  # should be a hash so the key can be a -1
-
-  9.times do |i|
-    neighbor_check = new_board_grid[i].map do |nc|
-      grab x and y of nc
-      do the math blindly and shove in array
-        screen out any "2" results
-        loop through new array matching x to nc[i][0] 
-          then matching "y"  get the nc[i][2] value
-          shove that value into array
-          count instances
-          apply rules
-          change or no change
-
-      
-
-  # upper_left = [1, 0], [1, -1], [0, -1]
-  # upper_right = upper_left.each { |ur| ur[0] = ur[0] * -1}
-  # puts upper_right
-
-
-  # upper_right = upper_left.each do |ur| { ur[0].to_i() * -1 }
-  # upper_right = upper_left.each { |ur| puts ur }
-
-  # board_grid
-  
-  # upper left
-  # x +1, y 0
-  # x +1, y -1
-  # x 0 y -1
-
-  # upper right x * -1
-
-  # x -1, y 0
-  # x -1, y -1
-  # x 0, y -1
-
-
-
-
-  #disposition handled here?
-  # should output CHANGE STATUS true or false
-  # check cell current state, if O or X
-  # O must have exactly 2-3 O neighbors OR x
-  # X with 4+ O neighbors TO O
-  # Another way to look at it is O is baseline?
-  @count.count # should total up the Os
+    
 
 end
 
-def corner_scan(orientation)
+def neighbor_scan(coordinates)
+  scan_template = [[-1, 1], [0, 1], [1, 1],
+                  [-1, 0], [0, 0], [1, 0],
+                  [-1, -1], [0, -1], [1, -1]]
+  current_zone = coordinates.split(", ")
+  scan_template.each do |st|
+    st[0] = st[0] + current_zone[0].to_i
+    st[1] = st[1] + current_zone[1].to_i
+  end
+  filter_template = scan_template.select { |a, b| a.abs != 2 && b.abs != 2 }
+  puts " filter_template "
+  puts filter_template
+  # ok, shift zero to the center zone of the grid, so the same method will for for checking all adjacent
+  # Move that into a collection
+  # Then remove from the collection anything with a "2"
+  # Use the remaining collection as coordinates to check zone values
+  # Push zone values into a different collection and total instances of O and X
+  # compare to the rules
+  # use compare results to modify the zone accordingly to O and X
+ 
 
 end
-
-def center_edge_scan(orientation)
-
-end
-
-
 
 
 #rudimentary UI
 
 system("clear")
 
-6.times { puts "" }
+3.times { puts "" }
 
 puts "\t\tGen 0\n"
 puts ""
 
-board_grid.each do |bg|
-    puts "\t\t" + bg.join(' ')
+board_grid.each_with_index do |(key, value), i|
+  # There's got to be a better way to do this
+  case i
+  when 0..1
+    top_row += "#{value} "
+  when 2
+    top_row += "#{value}"
+  when 3..4
+    mid_row += "#{value} "
+  when 5
+    mid_row += "#{value}"
+  when 6..7
+    bottom_row += "#{value} "
+  when 8
+    bottom_row += "#{value}"
+  end
 end
+
+puts "\t\t" + top_row
+puts "\t\t" + mid_row
+puts "\t\t" + bottom_row
 
 3.times { puts "" }
 
@@ -114,42 +87,26 @@ puts ""
 
 generate_next_gen(board_grid)
 
-@gen1_grid.each do |gg|
-    puts "\t\t" + gg.join(' ')
+@gen1_grid.each_with_index do |(key, value), i|
+  case i
+  when 0..1
+    g1_top_row += "#{value} "
+  when 2
+    g1_top_row += "#{value}"
+  when 3..4
+    g1_mid_row += "#{value} "
+  when 5
+    g1_mid_row += "#{value}"
+  when 6..7
+    g1_bottom_row += "#{value} "
+  when 8
+    g1_bottom_row += "#{value}"
+  end
 end
+
+puts "\t\t" + g1_top_row
+puts "\t\t" + g1_mid_row
+puts "\t\t" + g1_bottom_row
 
 3.times { puts "" }
-puts "HASH grid"
-puts ""
 
-(1..3).each do |i|
-  top_row += "#{new_board_grid[i][2]} "
-end
-puts top_row
-
-(4..6).each do |i|
-  mid_row += "#{new_board_grid[i][2]} "
-end
-puts mid_row
-
-(7..9).each do |i|
-  bottom_row += "#{new_board_grid[i][2]} "
-end
-puts bottom_row
-
-
-
-6.times { puts "" }
-
-
-# 9.times do |i|
-#   neighbor_check = new_board_grid[i].map do |nc|
-#     grab x and y of nc
-#     do the math blindly and shove in array
-#       screen out any "2" results
-#       loop through new array matching x to nc[i][0] 
-#         then matching "y"  get the nc[i][2] value
-#         shove that value into array
-#         count instances
-#         apply rules
-#         change or no change
