673356bd04554fd618a77f5b9c2fe0b6495c706e
Drew Peterson <drew.a.peterson@hotmail.com>
2022-02-03 02:51:01 -0800

    Added comments while things are fresh in my mind.  Updated and moved commented out checklist.

--- lib/conways-game-of-life.rb
+++ lib/conways-game-of-life.rb
@@ -6,6 +6,14 @@
                 # [-1, 0], [0, 0], [1, 0],
                 # [-1, -1], [0, -1], [1, -1]]
 
+# Originally was going to use an array of arrays, then after I stepped away from it,
+# I realized that I would have to have corner zone methods to check the surrounding values, 
+# side middle (such as -1, 0) methods and a middle method.  And those methods would have to rotate
+# 90 degrees as well.  
+# Then I realized that it, if I could make the center zone 0,0, then I just have to reject any 
+# out-of-bounds on a single 360 degree scan on each zone because every one of them would have a
+# 2 coordinate and I can filter out those.
+# So that made it essential to use a hash.
 @board_grid = { "-1, 1" => "O", "0, 1" => "X", "1, 1" => "X", 
               "-1, 0" => "X", "0, 0" => "O", "1, 0" => "O", 
               "-1, -1" => "O", "0, -1" => "O", "1, -1" => "X" }
@@ -16,6 +24,7 @@ top_row = ""; mid_row = ""; bottom_row = ""
 def generate_next_gen(grid)
   # I'd prefer not to destructively alter the original @board_grid for visual verfication
   # I don't like using "gen1_grid" as a name, wanted to make it flexible for multiple generation uses
+  # This method seems to be the best place to drive the overall script.
   @gen1_grid = Marshal.load(Marshal.dump(grid))
   @gen1_grid.each do |key, value|
     puts ""
@@ -27,7 +36,7 @@ def generate_next_gen(grid)
     # break # TO REMOVE
     # neighbor_scan(key)
   end
-    
+
 
 end
 
@@ -37,15 +46,23 @@ def neighbor_scan(coordinates, zone_value)
                   [-1, -1], [0, -1], [1, -1]]
   scan_coordinates = []
   scanned_values = []
-  current_zone = coordinates.split(", ")
-  scan_template.each do |st|
+
+  current_zone = coordinates.split(", ")  
+  # takes the argument for the zone we are currently in
+  scan_template.each do |st| 
+    # the scan_template checks each zone of the perimeter from 0,0.  
+    # This block adds the current coordinates to the scan template to scan each zone.
     st[0] = st[0] + current_zone[0].to_i
     st[1] = st[1] + current_zone[1].to_i
   end
+  # filter_template removes all the out of bounds zones the template generated
   filter_template = scan_template.select { |a, b| a.abs != 2 && b.abs != 2 }
+  # This puts together the arrays to more easily use them to match to the @board_grid keys
   filter_template.each do |ft|
     scan_coordinates << ft.join(", ")
   end
+  # This block finds the values surrounding each zone one at a time
+  # It stores all the values in the scanned_values array
   scan_coordinates.each do |sc|
     @board_grid.select do |key, value| 
       scanned_values << value if key == sc
@@ -57,13 +74,7 @@ def neighbor_scan(coordinates, zone_value)
   puts ""
   puts ""
   puts ""
-  # ok, shift zero to the center zone of the grid, so the same method will for for checking all adjacent
-  # Move that into a collection
-  # Then remove from the collection anything with a "2"
-  # Use the remaining collection as coordinates to check zone values
-  # Push zone values into a different collection and total instances of O and X
-  # compare to the rules
-  # use compare results to modify the zone accordingly to O and X
+
  
 
 end
@@ -79,7 +90,8 @@ puts "\t\tGen 0\n"
 puts ""
 
 @board_grid.each_with_index do |(key, value), i|
-  # There's got to be a better way to do this
+  # I wanted to see the original to visually verify what is working
+  # There's got to be a better way line the values in 3x3 rows
   case i
   when 0..1
     top_row += "#{value} "
@@ -130,3 +142,10 @@ puts "\t\t" + g1_bottom_row
 
 3.times { puts "" }
 
+  # DONE - ok, shift zero to the center zone of the grid, so the same method will for for checking all adjacent
+  # DONE - Move that into a collection
+  # DONE - Then remove from the collection anything with a "2"
+  # DONE - Use the remaining collection as coordinates to check zone values
+  # DONE - Push zone values into a different collection and total instances of O and X
+  # compare to the rules
+  # use compare results to modify the zone accordingly to O and X
